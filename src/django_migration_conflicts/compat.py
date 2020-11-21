396ab9a58275c913397bec98db2e79da4e8db5d5
Adam Johnson <me@adamj.eu>
2020-11-21 09:50:02 +0000

    more nocover

--- src/django_migration_conflicts/compat.py
+++ src/django_migration_conflicts/compat.py
@@ -3,7 +3,7 @@ import sys
 
 if sys.version_info >= (3, 6):  # pragma: no cover
     ModuleNotFoundError = builtins.ModuleNotFoundError
-else:
+else:  # pragma: no cover
     ModuleNotFoundError = ImportError
 
 if sys.version_info >= (3, 7):  # pragma: no cover
@@ -12,7 +12,7 @@ if sys.version_info >= (3, 7):  # pragma: no cover
         return module.__file__ is None
 
 
-else:
+else:  # pragma: no cover
 
     def is_namespace_module(module):
         return getattr(module, "__file__", None) is None
