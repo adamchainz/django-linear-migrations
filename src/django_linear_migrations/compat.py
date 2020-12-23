7c388a40886773f28321588dee01d7861b57344e
Adam Johnson <me@adamj.eu>
2020-12-23 10:41:54 +0000

    Move coverage to coveralls

--- src/django_linear_migrations/compat.py
+++ src/django_linear_migrations/compat.py
@@ -1,12 +1,12 @@
 import sys
 
-if sys.version_info >= (3, 7):  # pragma: no cover
+if sys.version_info >= (3, 7):
 
     def is_namespace_module(module):
         return module.__file__ is None
 
 
-else:  # pragma: no cover
+else:
 
     def is_namespace_module(module):
         return getattr(module, "__file__", None) is None
