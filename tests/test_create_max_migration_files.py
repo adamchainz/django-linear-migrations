405e926ddd912fb4015f132515df49fe42ff4666
Adam Johnson <me@adamj.eu>
2022-11-04 10:41:43 +0000

    Move from isort to reorder-python-imports
    It’s [way faster](https://twitter.com/codewithanthony/status/1553034384206438401) and its one-import-per-line style prevents merge conflicts.

--- tests/test_create_max_migration_files.py
+++ tests/test_create_max_migration_files.py
@@ -6,7 +6,8 @@ from io import StringIO
 
 import pytest
 from django.core.management import call_command
-from django.test import TestCase, override_settings
+from django.test import override_settings
+from django.test import TestCase
 
 
 class CreateMaxMigrationFilesTests(TestCase):
